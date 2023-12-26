f36f79c6f945efb1ae6554fed345d8785975cfe6
GhulamHaider79 <127304370+GhulamHaider79@users.noreply.github.com>
2023-12-26 19:07:57 +0500

    changes

--- assingnment4/app.js
+++ assingnment4/app.js
@@ -1,14 +1,14 @@
 
-//      Questin No 1
+//      Question No 1
 
 var arrMulti = [ [ [] ]]
 document.querySelector('.answer1').innerHTML = arrMulti;
 
-//    Questin No 2
+//    Question No 2
 
 var multiArray = [[0,1,2,3], [1,0,1,2], [2,1,0,1]];
 
-console.log( multiArray);
+console.log(multiArray);
 
 
 //var counter = document.querySelector('.answer2').innerText =  multiArray;
@@ -36,15 +36,28 @@ if (displayElement) {
 } else {
     console.error('Element with ID "matrixDisplay" not found.');
 }
-1
-
-
-var countingArrays = [1,2,3,4,5,6,7,8,9,10];
-
-var displayCountingArrays = document.querySelector('.answer3');
 
 
 
-for (let i = 1; i <= 10; i++) {
-    var displayCountingArrays = document.querySelector('.answer3').innerHTML=(i);
-}
+//    Question No 3
+
+// var countingArrays = [1,2,3,4,5,6,7,8,9,10];
+
+// var displayCountingArrays = document.querySelector('.answer3');
+
+// var countingStore = '';
+// if (displayCountingArrays){
+
+//    for (var i = 0; i < countingArrays.length; i++ ){
+//     countingStore =  countingArrays[i];
+
+    
+//      document.querySelector('.answer3').innerHTML =  `${countingStore} <br>`;
+  
+// }
+ 
+// }
+
+  for (let i = 1; i <= 10; i++) {
+        console.log(i);
+    }
